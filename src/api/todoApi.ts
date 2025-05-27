8b075d3409482f524ad6d975d3c18316949a81bd
honza250 <honzator500@seznam.cz>
2025-05-27 12:43:34 +0200

    oprava

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,6 +1,6 @@
 import type { Todo } from '../types'
 
-const API_URL = 'https://eli-workshop.vercel.app/api/users/xsmrj00/todos'
+const API_URL = 'https://eli-workshop.vercel.app/api/users/budj10/todos'
 
 class ApiErorr extends Error {
   constructor(message: string) {
